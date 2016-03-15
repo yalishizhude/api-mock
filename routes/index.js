8e8748e867c31aa4e59bff8d73d3cd42f048fcaf
Delong Zhu <Delong Zhu>
2016-03-15 21:27:58 +0800

    add jslintrc

--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,45 @@
 /*global module,require*/
-
-(function() {
-	'use strict';
-	var express = require('express');
-	var router = express.Router();
-	var Mock = require('mockjs');
-	var monk = require('monk');
-	var db = monk('localhost:27017/api');
-	var fs = require('fs');
-
-	var cInt = db.get('interfaces');
-	cInt.find({},{$ne:{valide:false}}, function(err, data){
-		if(err) throw err;
-		console.log('------', 'load interfaces start...');
-		var cnt = 0;
-		data.forEach(function(it){
-			if(it.method){
-				cnt++;
-				console.log(it.url,it.method);
-				router[it.method](it.url, function(req, res){
-					console.log(new Date(), req.path);
-					try{
-						req.session.ifcs = req.session.ifcs||[];
-						var data = Mock.mock(JSON.parse(it.outObject));
-						req.session.ifcs.push(req.path);
-						res.json(data);
-					} catch(e){
-						res.json(e);
-					}
-				});
-			}
-		});
-		console.log('------', 'loaded '+cnt+' interfaces~');
-	});
-	//通过重写文件引发服务器重启，从而重新加载接口
-	router.all('/rewrite/:timestamp', function(req, res){
-		fs.writeFile('./routes/timetamp.js', req.params.timestamp, 'utf8', function (err) {
-		  if (err) throw err;
-		});
-		res.json({});
-	});
-
-	router.get('/', function(req, res){
-		res.render('index',{title: 'api-mock-server'});
-	});
-
-	module.exports = router;
-}).call(this);
+(function () {
+  'use strict';
+  var express = require('express'),
+    router = express.Router(),
+    Mock = require('mockjs'),
+    monk = require('monk'),
+    db = monk('localhost:27017/api'),
+    cInt = db.get('interfaces');
+  cInt.find({}, {
+    $ne: {
+      valide: false
+    }
+  }, function (err, data) {
+    if (err) {
+      throw err;
+    }
+    console.log('------', 'load interfaces start...');
+    var cnt = 0;
+    data.forEach(function (it) {
+      if (it.method) {
+        cnt++;
+        console.log(it.url, it.method);
+        router[it.method](it.url, function (req, res) {
+          console.log(new Date(), req.path);
+          try {
+            req.session.ifcs = req.session.ifcs || [];
+            var data1 = Mock.mock(JSON.parse(it.outObject));
+            req.session.ifcs.push(req.path);
+            res.json(data1);
+          } catch (e) {
+            res.json(e);
+          }
+        });
+      }
+    });
+    console.log('------', 'loaded ' + cnt + ' interfaces~');
+  });
+  router.get('/', function (req, res) {
+    res.render('index', {
+      title: 'api-mock-server'
+    });
+  });
+  module.exports = router;
+}());
